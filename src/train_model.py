9ff7484080ba2797c15a9ea2644e501d9fd29ddb
madaankartik <kartikmadaan098@gmail.com>
2026-03-02 17:23:10 +0530

    python file to train the model is added inside src folder

--- src/train_model.py
+++ src/train_model.py
@@ -14,7 +14,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 def load_data(path):
     df = pd.read_csv(path)
 
-    # Handle missing values
+    # Handle missing value
     df["Battery Capacity (kWh)"] = df["Battery Capacity (kWh)"].fillna(
         df["Battery Capacity (kWh)"].median()
     )
